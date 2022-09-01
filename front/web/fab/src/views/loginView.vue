7c672beacc445e53db6274526db9a2144602a977
snow-sr <joaofelipi877@gmail.com>
2022-09-01 20:06:20 -0300

    aaa

---
+++ front/web/fab/src/views/loginView.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  methods: {
+    login() {
+      window.location.href = "http://localhost:8087/login";
+    },
+  },
+
+  mounted() {
+    this.login();
+  },
+};
+</script>
+
+<template>
+  <h1>You should not be here...</h1>
+</template>
